416d98d8e57ebeafc9db06e5837340b6a00d2366
blo0dyred <mahmoud.othman.ajam@gmail.com>
2021-08-04 21:21:21 +0300

    a working messaging function but needs more work on styling

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import "./App.css";
 import React from "react";
-import NavigationBar from "./Components/NavigationBar";
+import NavBar from "./Components/NavigationBar";
+// import ChatBox from "./Components/ChatBox";
+import {Launcher} from "react-chat-window";
 
 function App() {
-  return (
-    <div>
-      <NavigationBar mode="moderator"/>
-    </div>
-  );
+    return (
+      <div>
+        <NavBar mode="user"/>
+        <Launcher
+        agentProfile={{
+          teamName: "Chat Room",
+          imageUrl: "https://a.slack-edge.com/66f9/img/avatars-teams/ava_0001-34.png"
+        }}
+        onMessageWasSent={"send a message"}
+        messageList={[{author: "hehe",type: "text",data: {text: "blabla"}}]} showEmoji
+      />
+      </div>
+    );
 }
 
 export default App;
