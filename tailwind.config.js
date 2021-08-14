3fc5bcb7c66e88849471507164aac44a0bf12c0c
Ali Kabalan <ali.kabalan.1994@gmail.com>
2021-08-14 14:53:48 +0300

    finished tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: theme => ({
+        'contact-us-header': "url('contact-us-header.jpg')",
+       })
+    },
   },
   variants: {
     extend: {},
